795f2ef6c2e227ba2fedb9def61120dc7e1de282
smbct <buchet.s@laposte.net>
2017-05-15 09:45:08 +0200

    wrapper method

--- script/feature_selection.R
+++ script/feature_selection.R
@@ -106,7 +106,7 @@ feature_corr <- function(dataset, nfold) {
 
 }
 
-feature_PCA <- function(dataset) {
+feature_PCA <- function(dataset, nfold) {
 
     # number of variables
     n <- ncol(dataset) - 1
@@ -119,15 +119,16 @@ feature_PCA <- function(dataset) {
 
     X_pca <- data.frame(prcomp(dataset[, -(n+1)], retx=T)$x)
 
+
     # cross validation loop
     for (i in 1:nfold) {
 
         test_set_ind <- ((i-1)*CV_size+1):(i*CV_size)
-        X_test_set <- X_pca[test_set_ind]
+        X_test_set <- X_pca[test_set_ind,]
         Y_test_set <- dataset[test_set_ind, (n+1)]
 
         train_set_ind <- setdiff(1:N, test_set_ind)
-        X_train_set <- X_pca[train_set_ind]
+        X_train_set <- X_pca[train_set_ind,]
         Y_train_set <- dataset[train_set_ind, (n+1)]
 
         for(nb_features in 1:n) {
@@ -135,7 +136,7 @@ feature_PCA <- function(dataset) {
             # select only few columns
             DS<-cbind(X_train_set[, 1:nb_features, drop=F], Y_train_set)
 
-            model <- rpart(Y_train_set ~ ., DS)
+            model <- svm(Y_train_set ~ ., DS)
 
             Y_hat <- predict(model, X_train_set[,1:nb_features, drop=F])
 
@@ -147,8 +148,54 @@ feature_PCA <- function(dataset) {
     }
 }
 
-feature_wrap <- function(dataset) {
+feature_wrap <- function(dataset, nround, nfold) {
 
+    # number of variables
+    n <- ncol(dataset) - 1
+
+    # number of examples
+    N <- nrow(dataset)
+
+    # size of a test set
+    CV_size <- floor(N/nfold)
+
+    selected <- NULL
+
+    for (round in 1:nround) {
+
+        candidates <- setdiff(1:n, selected)
+
+        for (j in 1:length(candidates)) {
+
+            features_to_include <- c(selected, candidates[j])
+
+            for (i in 1:nfold) {
+
+                test_set_ind <- ((i-1)*CV_size+1):(i*CV_size)
+                X_test_set <- dataset[test_set_ind, features_to_include]
+                Y_test_set <- dataset[test_set_ind, (n+1)]
+
+                train_set_ind <- setdiff(1:N, test_set_ind)
+                X_train_set <- dataset[train_set_ind, features_to_include]
+                Y_train_set <- dataset[train_set_ind, (n+1)]
+
+                DS <- cbind(X_train_set, Y_train_set)
+                model <- lm(Y_train_set ~ ., DS)
+
+                Y_hat <- predict(model, X_test_set)
+
+                error <- sqrt( mean( (log(Y_hat) - log(Y_test_set))^2, na.rm=T ))
+
+                print(i)
+                print(error)
+
+            }
+
+
+        }
+
+
+    }
 
 
 }
