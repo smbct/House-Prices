61ad2762e3e8693c7571f588df5be74cf69f0eb3
smbct <buchet.s@laposte.net>
2020-03-29 18:33:02 +0200

    features

--- python/main.py
+++ python/main.py
@@ -13,7 +13,10 @@ from sklearn.neighbors import KernelDensity
 ######################################################################
 # gaussian based density estimation for the targer feature (SalePrice)
 ######################################################################
-def display_sell_price():
+def display_sell_price(ax):
+
+    # x axis in scientific notation
+    ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0), useMathText=True)
 
     saleprice_values = df_numerical[target_feature].values.copy()
 
@@ -39,12 +42,13 @@ def display_sell_price():
     lin_proba = np.exp(kde.score_samples(lin_values))*0.1
 
     plt.fill(np.concatenate((lin_values_2, lin_values_2[::-1])), np.concatenate((2-lin_proba, (2+lin_proba)[::-1]) ), facecolor='green', edgecolor='orangered', alpha=0.5, linewidth=1)
-    plt.scatter(saleprice_values, y_pos, s=1, marker = '.', c='k' )
+    plt.scatter(saleprice_values, y_pos, s=10, marker = '.')
     plt.boxplot(saleprice_values, vert=False, positions = [3], widths = [0.5], sym = 'r+')
 
     plt.ylim(0,4)
     plt.xlim(0, np.max(saleprice_values))
-    plt.title('SalePrice values')
+    plt.title('SalePrice distribution')
+
 
 
 
@@ -61,24 +65,67 @@ def plot_col_vs_target(dataframe, col, target_feature):
 ######################################################################
 # display categorical variables in a plot
 ######################################################################
-def display_categorical_feature(dataframe, cat_feature, target_feature):
-    print('hello')
+def display_categorical_feature(ax, dataframe, cat_feature, target_feature, mean_sort = False):
+
+     # x axis in scientific notation
+    ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0), useMathText=True)
 
     # extract the two columns
     df_temp = dataframe[[cat_feature, target_feature]]
+
+    # Check the presence of NA values
+    df_na = df_temp[cat_feature].isna()
+    has_na = df_na.any() # True iff there are na values in the dataframe 
     
+    if has_na:
+        df_temp[cat_feature].fillna('NA', inplace=True)
+        
     values = df_temp[cat_feature].values
-    
-    # for i in range(len(values)):
-        # if np.isnan(values[i]):
-            # values[i] = 'NA'
-
-    print(np.isnan(np.array(values)))
-
     values = np.unique(values)
 
-    print(cat_feature)
-    print(values)
+    if has_na: # 'NA' must be seperated
+        ind = np.where(values == 'NA')
+        values[ind] = values[-1]
+        values[-1] = 'NA'
+
+    lim = ax.get_xlim()
+    x_range = lim[1] - lim[0]
+
+    # compute positions for each categories
+    pos = [ind for ind in range(len(values))]
+    for ind in range(len(pos)):
+        pos[ind] = x_range/(len(pos))*(ind+0.5)
+
+    # order from mean
+    if mean_sort:
+        values_means = [-1 for ind in range(len(values))]
+        for ind in range(len(values)):
+            target_values = df_temp.loc[df_temp[cat_feature] == values[ind]][target_feature].values
+            values_means[ind] = np.median(target_values)
+        indices = np.argsort(values_means)
+        values = [values[ind] for ind in indices]
+
+    for ind in range(len(values)):
+        target_values = df_temp.loc[df_temp[cat_feature] == values[ind]][target_feature].values
+
+        X = [pos[ind] for j in range(len(target_values))]
+        # X += (np.random.rand(len(X)) - 0.5)*0.1
+
+        X += np.random.normal(0., 0.01, len(X))
+
+        ax.scatter(X, target_values, marker = '.', s = 5)
+        ax.boxplot(target_values, vert=True, positions=[pos[ind]], widths=[0.05], showfliers=False)
+
+    ax.set_xlim(lim)
+
+    # set x labels
+    print(pos)
+    ax.set_xticks(pos)
+    ax.set_xticklabels(values)
+
+    ax.set_title(target_feature + ' vs ' + cat_feature)
+
+    return
 
 # consider 0 as missing values: EnclosedPorch, BsmtFinSF2, 3SsnPorch, PoolArea, ScreenPorch, BsmtUnfSF (few), OpenPorchSF, 22ndFlrSF, WoodDeckSF, BsmtFinSF1, YearRemodAdd, TotalBsmtSF, GarageArea
 # create categorical variables when few values: KitchenAbvGr, LowQualFinSF, MiscVal, BsmtHalfBath, BsmtFullBath, HalfBath, Fireplaces, FullBath
@@ -127,12 +174,17 @@ target_feature = df_numerical.columns[-1]
 print('target feature: ' + target_feature)
 
 # plot data for SalePrice only (boxplot and scatter plot)
-plt.subplot(1,2,1)
-display_sell_price()
+ax = plt.subplot(2,2,1)
+display_sell_price(ax)
 
 # plot a categorical variable
-plt.subplot(1,2,2)
+ax = plt.subplot(2,2,2)
 # display_categorical_feature(df, cat_features[0], target_feature)
-display_categorical_feature(df, 'Alley', target_feature)
+# display_categorical_feature(ax, df, 'Alley', target_feature)
+
+display_categorical_feature(ax, df, cat_features[45], target_feature, mean_sort=True)
+
+# compute a UMAP on numerical features
+df_numerical.dropna(inplace=True)
 
 plt.show()
